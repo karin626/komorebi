5c871d34aef00b4e3d65d0324a37f42d94ba96ad
karin626 <68733251+karin626@users.noreply.github.com>
2020-09-18 11:53:44 +0900

    Add files via upload

--- css/javascript.js
+++ css/javascript.js
@@ -11,3 +11,17 @@ $(function() {
         }
     });
 });
+
+
+$(function(){
+    $(window).scroll(function (){
+        $('.left-to-right, .down-to-top').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 300){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
